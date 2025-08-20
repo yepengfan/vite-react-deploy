1b84c1362829bce1e6019285552edc89706e2e82
Ted Fan <ted.fan@flightcentre.com>
2025-08-20 21:35:34 +1000

    Add files

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "yepengfan.github.io/01-HTML/06-product-details-section/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
